4280a72c8320e60d510801d5c42cc9309c08dd84
Hamza <Hamza.klouch@wemoms.fr>
2023-06-13 15:45:27 +0200

    android: use stable version of agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id("com.android.application") version ("8.2.0-alpha06") apply false
+    id("com.android.application") version ("8.0.2") apply false
     id("org.jetbrains.kotlin.android") version ("1.8.21") apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
