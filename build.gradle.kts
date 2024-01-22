90ac5eb6a5fc2d72ef6dd201b1e5b1eaf75c6759
Hamza <Hamza.klouch@wemoms.fr>
2024-01-22 17:41:14 +0100

    bump kotlin to 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     //FIXME Find a way to reuse versions from the gradle catalog while allowing
     // the example project to be built in a standalone manner
     id("com.android.application") version ("8.2.1") apply false
-    id("org.jetbrains.kotlin.android") version ("1.9.20") apply false
+    id("org.jetbrains.kotlin.android") version ("1.9.22") apply false
 }
 //true // Needed to make the Suppress annotation work for the plugins block
