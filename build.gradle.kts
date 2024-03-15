5d02b77f4a2f5df67bc9bb098d6aedf6d36204b3
Hamza <Hamza.klouch@wemoms.fr>
2024-03-15 08:46:03 +0100

    bump compose compiler and gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     //FIXME Find a way to reuse versions from the gradle catalog while allowing
     // the example project to be built in a standalone manner
-    id("com.android.application") version ("8.2.2") apply false
+    id("com.android.application") version ("8.3.0") apply false
     id("org.jetbrains.kotlin.android") version ("1.9.22") apply false
 }
 //true // Needed to make the Suppress annotation work for the plugins block
